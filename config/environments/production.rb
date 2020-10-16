f834399c7eb2bbd093aa8419f9cfa38533555f47
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-16 14:41:13 -0400

    [HOLR-1493] us ws:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = "wss://#{ENV.fetch('BACKEND_DOMAIN')}/cable"
+  config.action_cable.url = "ws://#{ENV.fetch('BACKEND_DOMAIN')}/cable"
   config.action_cable.allowed_request_origins = [ENV.fetch('FRONTEND_DOMAIN')]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
