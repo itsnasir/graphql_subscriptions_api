ac71ce0e4eb354f3bfde4bce8da3e925bea65829
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-19 11:42:48 -0400

    [HOLR-1493] Use ENV variable for allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = '/cable'
   config.action_cable.url = "wss://#{ENV.fetch('BACKEND_DOMAIN')}/cable"
-  config.action_cable.allowed_request_origins = ['localhost:3001', ENV.fetch('FRONTEND_DOMAIN')]
+  config.action_cable.allowed_request_origins = [ENV.fetch('FRONTEND_DOMAIN')]
   config.action_cable.disable_request_forgery_protection = true
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
