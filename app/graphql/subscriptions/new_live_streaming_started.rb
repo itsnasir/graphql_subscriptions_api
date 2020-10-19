1de5e266e3858a6357810adf3d839464541e2782
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-19 10:45:55 -0400

    [HOLR-1493] Add method for new_live_streaming

--- app/graphql/subscriptions/new_live_streaming_started.rb
+++ app/graphql/subscriptions/new_live_streaming_started.rb
@@ -1,3 +1,7 @@
 class Subscriptions::NewLiveStreamingStarted < Subscriptions::BaseSubscription
   field :new_live_streaming, Types::LiveStreamingType, null: true
+
+  def new_live_streaming
+    object
+  end
 end
