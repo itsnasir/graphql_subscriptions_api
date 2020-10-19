6b75c14d8d8ac3b498edaa57863b33a86a08f54a
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-19 08:39:43 -0400

    [HOLR-1493] move action cable mount path to route.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = '/cable'
+  # config.action_cable.mount_path = '/cable'
   config.action_cable.url = "wss://#{ENV.fetch('BACKEND_DOMAIN')}/cable"
   config.action_cable.allowed_request_origins = [ENV.fetch('FRONTEND_DOMAIN')]
 
