e7f093429ac1db9f286a65af5a6392ffdcf777c2
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-19 11:54:27 -0400

    [HOLR-1493] add new_comment subscrption method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   def notify_subscriber_of_addition
     GraphqlSubscriptionsSchema.subscriptions.trigger('newCommentPosted', { live_streaming_id: live_streaming_id }, self)
   end
+
+  def new_comment
+    {
+      id: id,
+      content: content,
+      live_streaming_id: live_streaming_id
+    }
+  end
 end
