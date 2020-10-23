8e645f8a77a2a58f03aa2d17e0ed7e5f4b735bfe
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-23 13:53:50 -0400

    [HOLR-1455] Register subscribers

--- app/graphql/subscriptions/new_live_streaming_started.rb
+++ app/graphql/subscriptions/new_live_streaming_started.rb
@@ -1,3 +1,17 @@
 class Subscriptions::NewLiveStreamingStarted < Subscriptions::BaseSubscription
   field :new_live_streaming, Types::LiveStreamingType, null: true
+
+  def subscribe
+    subscribers_tracker.subscribe
+  end
+
+  def unsubscribe
+    subscribers_tracker.unsubscribe
+  end
+
+  private
+
+  def subscribers_tracker
+    ::SubscribersTracker.new('new_live_streaming')
+  end
 end
--- app/graphql/types/live_streaming_type.rb
+++ app/graphql/types/live_streaming_type.rb
@@ -1,6 +1,11 @@
 module Types
   class LiveStreamingType < Types::BaseObject
-    field :id,  Integer, null: false
-    field :url, String,  null: false
+    field :id,                Integer, null: false
+    field :url,               String,  null: false
+    field :subscribers_count, Integer, null: true
+
+    def subscribers_count
+      SubscribersTracker.current_subscribers_count('new_live_streaming')
+    end
   end
 end
